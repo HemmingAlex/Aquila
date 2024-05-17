fdcc11df3dc05765fd904967e9d736675298f689
Alex <alexander.hemming@gmail.com>
2024-05-17 11:55:43 +0100

    nodemail

--- pages/api/SendMessage.ts
+++ pages/api/SendMessage.ts
@@ -1,14 +1,8 @@
-// import { supabase } from "../../data/initSupabase";
 const nodemailer = require("nodemailer");
 
 export default async function handler(req:any, res:any) {
   if (req.method === "POST") {
-    const body = req.body;
-
-    // const { data, error } = await supabase.from("Messages").insert(body);
-
-    // if (!error) {
-      // Send an email
+      const body = req.body;
       const {name, email,options, message } = body;
 
       const transporter = nodemailer.createTransport({
@@ -35,24 +29,8 @@ export default async function handler(req:any, res:any) {
 
       await transporter.sendMail(mailData);
 
-      // res.status(200).json(data);
     } else {
       console.log('fook')
-      // res.status(500).json(error);
     }
   }
-  // if (req.method === "DELETE") {
-  //   const { id } = req.body;
 
-  //   const { data, error } = await supabase
-  //     .from("Messages")
-  //     .delete()
-  //     .match({ id });
-
-  //   if (!error) {
-  //     res.status(200).json(data);
-  //   } else {
-  //     res.status(500).json(error);
-  //   }
-  // }
-// }
