f06ac29308833e8a1d41225d35cacb4bf4c10014
Alex <alexander.hemming@gmail.com>
2024-05-17 11:53:39 +0100

    revet thing un  needed

--- plasmic-init.ts
+++ plasmic-init.ts
@@ -1,5 +1,4 @@
 import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
-import ProductCard from '../components/ProductCard';
 
 export const PLASMIC = initPlasmicLoader({
   projects: [
@@ -25,37 +24,3 @@ export const PLASMIC = initPlasmicLoader({
 
 // PLASMIC.registerComponent(...);
 
-PLASMIC.registerComponent(ProductCard, {
-  name: "ProductCard",
-  props: {
-    // Pass in arbitrary content in the visual editing canvas
-    children: 'slot',
-    
-    // You can have any number of slots.
-    header: 'slot',
-    
-    // Simple scalar props
-    productId: 'string',
-    darkMode: 'boolean',
-    
-    // Some props can take an enum of options
-    elevation: {
-      type: 'choice',
-      options: ['high', 'medium', 'flat']
-    }
-    
-    // Some props can take an arbitrary JSON object
-    config: 'object',
-    
-    // Some props can have dynamic configurations
-    headerColor: {
-      type: 'choice',
-      
-      // Hide the 'color' prop if no header content
-      hidden: (props:any) => !props.header,
-      
-      // Offer different choices depending on if darkMode is on
-      options: (props:any) => props.darkMode ? ['black', 'blue'] : ['yellow', 'green']
-    }
-  }
-});
